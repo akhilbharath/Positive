e9350381153f7385d3ac9bcd83f7611b63fe6d5b
Akhil Bharath <63289606+akhilbharath@users.noreply.github.com>
2020-04-07 15:33:41 +0530

    Add files via upload

---
+++ Positive range.py
@@ -0,0 +1,10 @@
+list1 = (12,-7,5,64,-14)
+print('list1')
+for real in list1:
+    if real>=0:
+        print (real)
+list2 = (12,14,-95,3)
+print('list2')
+for number in list2:
+    if number>=0:
+        print(number)
